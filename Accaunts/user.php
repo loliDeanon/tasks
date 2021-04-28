9a06cee31c8dddccf55222a6d69b37acaea472fa
loliDeanon <82580177+loliDeanon@users.noreply.github.com>
2021-04-28 14:08:31 +0300

    Update user.php

--- Accaunts/user.php
+++ Accaunts/user.php
@@ -1,16 +1,41 @@
 <?php
-class authorization {
+require_once 'D:\Mihaylov\task\MVC\Module\DataBase\DB.php';
+use DB;
+// Класс для авторизации 
+class authorization extends DB{
+    // Функцияя для получения данныъх о логине и пароле 
     function getUserData(){
-        $name = "не определено";
-        $age = "не определен";
         if(isset($_POST["login"])){
         
-            $name = $_POST["login"];
+          return  $login = $_POST["login"];
+        }
+        else {
+            echo 'Введите логин';
         }
         if(isset($_POST["pass"])){
         
-            $age = $_POST["pass"];
+         return   $pass = $_POST["pass"];
+        }
+        else {
+            echo 'Введите пароль';
+        }
+    }
+    // Функция для проверки на наличие логина и пароля в базе данных
+    function ExistenceСheck ($login,$pass){
+        $Checked = false;
+        $mysql->connect('localhost', 'root', 'lag123', 'tasks', '3306');
+        $Check = "select * from users where `login`=$login and pass = $pass;"
+        DB::select($Check);
+        if ($Checked === true){
+            echo "вы вошли"
+        }
+        else {
+            echo "error"
         }
     }
 }
+// вузов функций 
+$input = new authorization;
+$input -> getUserData();
+$input -> ExistenceСheck ($login,$pass);
 // echo "Имя: $name <br> Возраст: $age";
